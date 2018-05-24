eade3ea9aa96b76288cba39dbba8e0a5d77e8bec
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2018-05-24 12:04:25 +0200

    initial

---
+++ src/main/java/com/github/michalszynkiewicz/demo/foo/MyUtils.java
@@ -0,0 +1,14 @@
+package com.github.michalszynkiewicz.demo.foo;
+
+/**
+ * mstodo: Header
+ *
+ * @author Michal Szynkiewicz, michal.l.szynkiewicz@gmail.com
+ * <br>
+ * Date: 5/24/18
+ */
+public class MyUtils {
+    public static void printStackTrace() {
+        Thread.dumpStack();
+    }
+}
